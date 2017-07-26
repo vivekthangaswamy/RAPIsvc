24450769777e453892540fb09e60954f820159ab
Vivek <tvivekt@hotmail.com>
2017-07-26 22:25:46 +1000

    Add project files.

---
+++ RAPIsvc/IRAPIsvc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAPIsvc
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRAPIsvc" in both code and config file together.
+    [DataContractAttribute(Name = "TriangleType", Namespace = "http://KnockKnock.readify.net")]
+    public enum TriangleType : int
+    {
+
+        [EnumMemberAttribute()]
+        _Error = 0,
+
+        [EnumMemberAttribute()]
+        _Equilateral = 1,
+
+        [EnumMemberAttribute()]
+        _Isosceles = 2,
+
+        [EnumMemberAttribute()]
+        _Scalene = 3,
+    }
+
+    [ServiceContractAttribute(Namespace = "http://KnockKnock.readify.net", ConfigurationName = "IRedPill")]
+    public interface IReadifyInterviewService
+    {
+        [OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/WhatIsYourToken", ReplyAction = "http://KnockKnock.readify.net/IRedPill/WhatIsYourTokenResponse")]
+        Guid WhatIsMyToken();
+
+        [OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/WhatShapeIsThis", ReplyAction = "http://KnockKnock.readify.net/IRedPill/WhatShapeIsThisResponse")]
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+        [OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumber", ReplyAction = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberResponse")]
+        [FaultContractAttribute(typeof(System.ArgumentOutOfRangeException), Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberArgumentOutOfRangeException" +
+            "Fault", Name = "ArgumentOutOfRangeException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        long FibonacciNumber(long n);
+
+        [OperationContractAttribute(Action = "http://KnockKnock.readify.net/IRedPill/ReverseWords", ReplyAction = "http://KnockKnock.readify.net/IRedPill/ReverseWordsResponse")]
+        [FaultContractAttribute(typeof(System.ArgumentNullException), Action = "http://KnockKnock.readify.net/IRedPill/ReverseWordsArgumentNullExceptionFault", Name = "ArgumentNullException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        string ReverseWords(string s);
+    }
+}
---
+++ RAPIsvc/RAPIsvc.svc.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAPIsvc
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class RAPIsvc : IReadifyInterviewService
+    {
+        public Guid WhatIsMyToken()
+        {
+            return new Guid("eb65085d-699c-4c69-ab0e-6c8d38160997");
+        }
+
+
+        public TriangleType WhatShapeIsThis(int x, int y, int z)
+        {
+            if ((x + y > z && x + z > y && y + z > x) && (x > 0 && y > 0 && z > 0))
+            {
+                if (x == y && y == z)
+                {
+                    return TriangleType._Equilateral;
+                }
+                else if (x == y || y == z || x == z)
+                {
+                    return TriangleType._Isosceles;
+                }
+                else
+                {
+                    return TriangleType._Scalene;
+                }
+            }
+            else
+            {
+                return TriangleType._Error;
+            }
+        }
+
+
+        public long FibonacciNumber(long c)
+        {
+            long counter = c;
+            var fibonacci = new List<long>();
+
+            if (c == 0)
+            {
+                return 0;
+            }
+            //check for negative value, 
+            //if it is negative convert to positive by multiplying by -1
+            if (c < 0)
+            {
+                counter = counter * -1;
+            }
+
+
+            fibonacci.Add(0);
+            fibonacci.Add(1);
+
+            for (int i = 2; i <= counter; i++)
+            {
+                fibonacci.Add(fibonacci[i - 1] + fibonacci[i - 2]);
+            }
+
+            var result = fibonacci[fibonacci.Count - 1];
+
+            if (c < -1)
+            {
+                result = result * -1;
+            }
+            return result;
+
+        }
+
+        public string ReverseWords(string s)
+        {
+            string temp = "", result = "";
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] != ' ')
+
+                    temp = s[i] + temp;
+                else
+                {
+                    result += temp + s[i];
+                    temp = "";
+                }
+
+            result += temp;
+
+            return result;
+        }
+
+      
+    }
+}
